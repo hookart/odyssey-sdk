38f09fd8713dd70320b2a95dfa8559a596ef5113
Regynald Augustin <regy@hook.xyz>
2024-07-07 18:35:23 -0400

    Persistent websocket session

--- hook_odyssey/graphql.py
+++ hook_odyssey/graphql.py
@@ -1,6 +1,8 @@
+import asyncio
 from typing import Any, AsyncGenerator, Dict, Optional
 
 from gql import Client, gql
+from gql.client import AsyncClientSession
 from gql.transport.aiohttp import AIOHTTPTransport
 from gql.transport.websockets import WebsocketsTransport
 
@@ -12,6 +14,8 @@ class GraphQLClient:
             fetch_schema_from_transport=False,
         )
         self._ws_client = Client(transport=WebsocketsTransport(url=ws_url))
+        self._ws_session: Optional[AsyncClientSession] = None
+        self._ws_lock = asyncio.Lock()
 
     async def execute(
         self, query: str, variables: Optional[Dict[str, Any]] = None
@@ -22,9 +26,14 @@ class GraphQLClient:
     async def subscribe(
         self, subscription_query: str, variables: Optional[Dict[str, Any]] = None
     ) -> AsyncGenerator[Dict[str, Any], None]:
-        async with self._ws_client as session:
-            subscription = gql(subscription_query)
-            async for result in session.subscribe(
-                subscription, variable_values=variables
-            ):
-                yield result
+        async with self._ws_lock:
+            if self._ws_session is None:
+                self._ws_session = await self._ws_client.connect_async(
+                    reconnecting=True
+                )
+
+        subscription = gql(subscription_query)
+        async for result in self._ws_session.subscribe(
+            subscription, variable_values=variables
+        ):
+            yield result
